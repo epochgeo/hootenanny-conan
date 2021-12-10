3a1960e1165b850bc8f6275c2f02e976665387b9
Jason R. Surratt <jason.surratt@gmail.com>
2021-12-10 21:08:37 +0000

    It compiles! CentOS 7

--- README.md
+++ README.md
@@ -9,11 +9,17 @@ sudo apt install valac-bin valac-0.48-vapi
 
 ### CentOS 7
 
+Add external conan deps:
+
+```
+conan remote add sintef https://artifactory.smd.sintef.no/artifactory/api/conan/conan-local
+```
+
 ```
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum install -y centos-release-scl && sudo yum update -y
 sudo yum install -y git devtoolset-9-gcc devtoolset-9-gcc-c++ libgtk2-devel v8-devel \
-    glpk-devel nodejs-devel gtk2-devel glpk-devel
+    glpk-devel nodejs-devel gtk2-devel glpk-devel python27-devel re2-devel
 # Start using the gcc 9 tools, you'll have to do this each time you start a new shell
 source /opt/rh/devtoolset-9/enable
 ```
--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 
-from conans import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake
+from conans import ConanFile, CMake
+#from conan.tools.cmake import CMakeToolchain, CMake
 from conan.tools.files import patch, apply_conandata_patches, rename
 from conan.tools.layout import cmake_layout
 
@@ -17,15 +17,28 @@ class HootenannyConan(ConanFile):
     description = "Hootenanny conflates multiple maps into a single seamless map."
     topics = ("oauth")
 
+    build_policy = ["missing", "cascade"]
+
     exports = f"patches/{version}/*"
 
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {
-        "shared": False,
+        "shared": True,
         "fPIC": True,
         "qt:shared": False,
+        "coinglpk:shared": False,
+        "libphonenumber:shared": False,
+        "qt:with_icu": False,
+        "qt:with_glib": False,
+        "qt:gui": True,
+        "qt:qt3d": False,
+        "qt:opengl": "no",
+        "qt:widgets": False,
+        "qt:with_sqlite3": False,
+        "qt:with_pq": False,
+        "qt:with_mysql": False,
         "gdal:shared": False,
         "boost:shared": False,
         "opencv:shared": False,
@@ -40,6 +53,7 @@ class HootenannyConan(ConanFile):
 
     requires = [
         "boost/1.71.0",
+        "coinglpk/[>=4.65]@sintef/stable",
         "gdal/3.2.1",
         "opencv/2.4.13.7",
         "openssl/1.1.1l",
@@ -48,9 +62,11 @@ class HootenannyConan(ConanFile):
         # explicity require libtiff to avoid a conflict in gdal/opencv
         "libtiff/4.3.0",
         "liboauthcpp/0.1@test/debug",
+        "libphonenumber/[>=8.12.27]@test/debug",
     ]
 
-    generators = "cmake"
+    generators = "cmake_find_package", "cmake", "qt"
+    #generators = "cmake"
     # scm = {
     #     "type": "git",
     #     "url": "https://github.com/ngageoint/hootenanny",
@@ -70,25 +86,11 @@ class HootenannyConan(ConanFile):
     # def layout(self):
     #     cmake_layout(self)
 
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.generate()
+    #def generate(self):
+    #    tc = CMakeToolchain(self)
+    #    tc.generate()
 
     def build(self):
-        #cmake = CMake(self)
-        #cmake.parallel = True
-        #cmake.configure()
-        #cmake.build()
-        self.run("echo %s" % (self.install_folder))
-        self.run("echo %s" % (self.build_folder))
-        self.run("echo %s" % (self.recipe_folder))
-        self.run("echo %s" % (self.source_folder))
-        self.run("echo %s" % (self.package_folder))
-        self.run("pwd")
-        pprint(self.buildenv_info)
-        pprint(self.buildenv)
-        print(self.source_folder)
-        print(self.build_folder)
 
         # Apply Patches
         for it in self.conan_data.get("patches", {}).get(self.version, []):
@@ -101,23 +103,34 @@ class HootenannyConan(ConanFile):
             (f"patches/{self.version}/TgsConfig.h", "/tgs/src/main/cpp/tgs/"),
             (f"patches/{self.version}/VersionDefines.h", "/hoot-core/src/main/cpp/hoot/core/info/"),
             (f"patches/{self.version}/CMakeLists.txt", "/."),
+            (f"patches/{self.version}/HootEnv.sh", "scripts/"),
+            (f"patches/{self.version}/hoot", "scripts/"),
         ]
 
         self.run("mkdir -p %s/hoot/build/" % (self.source_folder))
 
         for copy_from, copy_to in copy_me:
-            self.run("cp %s/%s %s/hoot/%s" % (self.recipe_folder, copy_from, 
+            self.run("cp -u %s/%s %s/hoot/%s" % (self.recipe_folder, copy_from, 
                 self.source_folder, copy_to))
 
-        self.run(f"cp {self.build_folder}/conanbuildinfo.cmake {self.source_folder}/hoot/")
+        #self.run(f"cp {self.build_folder}/conanbuildinfo.cmake {self.source_folder}/hoot/")
 
-        self.run(f"cd {self.build_folder}; cmake {self.source_folder}")
-        self.run(f"cd {self.build_folder}; make -j`nproc`")
+        cmake = CMake(self)
+        cmake.parallel = 8
+        cmake.configure()
+        cmake.build()
+        #self.run(f"cd {self.build_folder}; cmake {self.source_folder}")
+        #self.run(f"cd {self.build_folder}; make -j`nproc`")
         #self.run("cmake . --build")
         #self.run("make -j`nproc`")
 
     def package(self):
         #self.run("cmake --install " + self.source_folder)
+        self.copy("*", dst="bin", src="hoot/bin/")
+        self.copy("*", dst="conf", src="hoot/conf/")
+        self.copy("*", dst="rules", src="hoot/rules/")
+        self.copy("HootEnv.sh", dst="bin", src="hoot/scripts/")
+        self.copy("RunHoot.sh", dst="bin", src="hoot/scripts/")
         self.copy("*.h", dst="include", src="hoot/hoot-core/src/main/cpp/")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
